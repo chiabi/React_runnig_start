76eabc420c5966d28f26c51b197ace4e4a914306
chiabi <chiabi88@gmail.com>
2018-05-02 01:27:33 +0900

    1. 리액트 기초다지기 ~ 2.4. 클래스 메서드 바인딩

---
+++ hackernews/src/App.js
@@ -0,0 +1,57 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+const list = [
+  {
+    title: 'React',
+    url: 'https://reactjs.org/',
+    author: 'Jordan Walke',
+    num_comments: 3,
+    points: 4,
+    objectID: 0,
+  },
+  {
+    title: 'Redux',
+    url: 'https://github.com/reactjs/redux',
+    author: 'Dan Abramov, Andrew Clark',
+    num_comments: 2,
+    points: 5,
+    objectID: 1,
+  },
+];
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      list,
+    }
+    this.onDismiss = this.onDismiss.bind(this);
+  }
+  onDismiss(id) {
+    const isNotId = item => item.objectID !== id;
+    const updatedList = this.state.list.filter(isNotId);
+    this.setState({list: updatedList});
+  }
+  render() {
+    return (
+      <div className="APP">
+        {this.state.list.map(item => 
+            <div key={item.objectID}>
+              <span><a href={item.url}>{item.title}</a></span>
+              <span>{item.author}</span>
+              <span>{item.num_comments}</span>
+              <span>{item.points}</span>
+              <button 
+                onClick={() => this.onDismiss(item.objectID)} 
+                type="button"
+              >dismiss</button>
+            </div>
+        )}
+      </div>
+    );
+  }
+}
+
+export default App;
